67a96246f74c8b04e6099c083246c360a3731483
Samir Ahmed <samirahmed2013@gmail.com>
2014-01-08 22:29:56 -0800

    Working version
    - app name is required (for GA 'App' type)
    - streamlined debug trace and err/exceptions
    - enable issuing request on new thread
    - first thread returns in under 1ms!

--- lib/analytics.rb
+++ lib/analytics.rb
@@ -2,21 +2,24 @@ class Analytics
    class << self
     require  'net/http'
     require  'socket'
-    require  'uri'
+    require  'open-uri'
     require  'securerandom'
 
-    attr_accessor :debug_mode, :raise_errors, :app_name, :app_version, :client_id, :tracking_id, :protocol_version, :anonymize_ip
+    attr_accessor :debug, :raising,                                        # Gem Settings
+                  :app_name, :client_id, :tracking_id, :protocol_version,  # G.A Required
+                  :anonymize_ip, :session, :app_version                    # G.A Optional
 
-    def configure(tid, opts = {})
-     
-      # set tracking id, ensure is not nil
+    def configure(tid, appname,  opts = {})
+
+      # set tracking id and appname, required!
       @tracking_id = tid
+      @app_name = appname
 
       # by default ignore errors
-      @ignore_errors = false
+      @raise = false
       
-      # set debug mode to be true
-      @debug = false
+      # configure settings
+      @debug = @debug
 
       # Generate a random client Id
       @client_id = SecureRandom.uuid()
@@ -27,25 +30,29 @@ class Analytics
         return unless self.respond_to?(key.to_s+"=")
         self.send(key.to_s+"=",value)
       end
-
+      
+      # Return the configuration hash
       globals
-    end
 
-    def debug_print( msg )
-      (puts msg) if @debug_mode
     end
 
     [:event, :exception].each do |method|
       define_method(method) do |*args|
         begin
-          self.class.send("#{method}!".to_sym, *args)
-        rescue
+          self.send("#{method}!".to_sym, *args)
+          puts (Time.now-start).to_f * 1000.0
+        rescue Exception => e
+          trace e.message
+          trace e.backtrace.inspect
           nil
         end
       end
     end
 
     def event!( category, action, opts = {})
+      puts category
+      puts action
+      puts opts.inspect
       execute opts.merge({ 
         category:category, 
         action:action,
@@ -67,12 +74,10 @@ class Analytics
       Hash[instance_variables.map{ |name| [name[1..-1].to_sym, instance_variable_get(name)]}]
     end
 
-    ENDPOINT = "http://www.google-analytics.com/collect"
-
     GLOBAL_OPT = {
       :session => {key:"sc", nominal:["start", "end"]},
       :anonymize_ip => {key:"aip", value:"1"},
-      :app_name => {key:"an", byte:100},
+      :app_name => {key:"an", byte:100, required:true},
       :app_version => {key:"av", byte:100},
       :non_interaction => {key:"ni", value:"1"},
       :client_id => {key:"cid", required:true},
@@ -103,23 +108,20 @@ class Analytics
 
       # Get option default value, or user specified value
       _value = opt[:value] || value
-      
-      # Ensure non null on optional values
-      if _value.nil? && opt[:required]
-        raise "No Value Provided for Required Parameter #{_key}" unless @ignore_errors
+     
+      result = {}
+
+      if _value.nil?
+        err "nil value for required parameter #{_key}" if opt[:required]
+      elsif opt[:byte] && _value.bytesize > opt[:byte]
+        err "#{_value} is larger than #{opt[:byte]} bytes"
+      elsif opt[:nominal] && !opt[:nominal].include?(_value) 
+        err "#{_key} must equal #{opt[:nominal].inspect} - not #{_nominal}"
+      elsif _value
+        result = {_key => _value}
       end
 
-      # Ensure the byte limit is not exceed
-      if opt[:byte] && _value.bytesize > opt[:byte]
-        raise "#{_value} is larger than #{opt[:byte]} bytes" unless @ignore_errors
-      end
-
-      # Ensure the value is one of the accepted values
-      if opt[:nominal] && !opt[:nominal].include?(_value) 
-        raise "#{_nominal}" unless ignore?
-      end
-
-      return {_key => _value}
+      result
     end
     
     def transform(original, options)
@@ -130,39 +132,47 @@ class Analytics
    
     def execute(opts)
       
-      return false if @tracking_id.nil? && !(@tracking_id =~ /UA-\w{4,8}-\w{1,2}/)
-      
-      params = globals
-      params.merge! opts
-      params = transform( params, GLOBAL_OPT )
+      err "Invalid TrackingId" unless @tracking_id && (@tracking_id =~ /UA-\w{4,8}-\w{1,2}/)
+
+      query = globals
+      query.merge! opts
+      query = transform( query, GLOBAL_OPT )
 
       case opts[:hit_type]
         when "event"
-          params.merge! transform(opts, EVENT_OPT)
+          query.merge! transform(opts, EVENT_OPT)
         when "exception"
-          params.merge! transform(opts, EXCEPTION_OPT)
+          query.merge! transform(opts, EXCEPTION_OPT)
       end
       
-      debug_print params.inspect 
+      trace query.inspect 
       
-      #Thread.new do
+      thread = Thread.new do
         
-        _uri = URI(ENDPOINT)
-        _http = Net::HTTP.new(_uri.host, _uri.port)
-        
-        _uri.query = URI.encode_www_form( params )
-        _req = Net::HTTP::Get.new(_uri.request_uri)
+        _uri = URI::HTTP.build({
+          :host => "www.google-analytics.com",
+          :path => "/collect",
+          :query => query.map{|k,v| "#{k}=#{URI.escape(v)}"}.join('&')
+        })
 
-        # todo add headers here ...
-        debug_print _uri.to_s 
-        _resp = _http.request(_req)
+        trace _uri.to_s 
+        _resp = Net::HTTP.get_response(_uri)
 
-        debug_print _resp.code
-        _resp.each{ |name, value| debug_print "#{name}:#{value}" }
-      #end
-
-      true
-
+        trace _resp.code
+        _resp.each{ |name, value| trace "#{name}:#{value}" }
+      end
+      
+      thread
     end
+
+    def trace msg
+      puts(msg) if @debug
+    end
+
+    def err msg 
+      trace msg 
+      raise msg unless @raise
+    end
+
   end
 end
